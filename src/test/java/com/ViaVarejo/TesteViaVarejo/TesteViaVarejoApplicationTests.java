35577719c90579ec8dbadd3bd3b17cadd29982ca
jojoccp <jonatas.ccp@gmail.com>
2021-05-09 20:17:15 -0300

    spring stater configurado

---
+++ src/test/java/com/ViaVarejo/TesteViaVarejo/TesteViaVarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ViaVarejo.TesteViaVarejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteViaVarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
